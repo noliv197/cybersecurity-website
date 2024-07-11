f39925fafdf42d6168520a42badc93e37ed8ff8b
natalia <>
2024-07-11 16:43:09 -0700

    Project initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserPage from "./pages/UserPage";
+import NotFoundPage from "./pages/404";
+import AdminPage from "./pages/AdminPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' >
+          <Route index element={<UserPage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='/register' element={<RegisterPage/>}/>
+        </Route>
+        <Route path='/admin' >
+          <Route path='home' element={<AdminPage/>}/>
+        </Route>
+        <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,6 @@
+export default function RegisterPage(props){
+    return(
+        <>
+        </>
+    )
+}
